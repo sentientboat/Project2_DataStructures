27fe1ad10488dfb2170077dae5594d960d2d4295
sentientboat <sentientboat@protonmail.com>
2019-06-10 10:28:11 +0200

    blockchain completed

--- 5_Blockchain.py
+++ 5_Blockchain.py
@@ -1,21 +1,23 @@
 import hashlib
 import datetime
+import time
 
 
 class Block:
 
     def __init__(self, timestamp, data, previous_hash):
-        self.timestamp = timestamp
+        self.timestamp = str(timestamp)
         self.data = data
         self.previous_hash = previous_hash
         self.hash = self.calc_hash()
 
     def calc_hash(self):
         sha = hashlib.sha256()
-        for i in (self.timestamp, self.data):
+        for i in [self.timestamp, self.data]:
+            print("encoding this...{}".format(i))
             enc = str(i).encode('utf-8')
             sha.update(enc)
-            return sha.hexdigest()
+        return sha.hexdigest()
 
 
 class BlockChain:
@@ -32,10 +34,12 @@ class BlockChain:
             previous_block = self.head
             new_block = Block(timestamp, data, previous_block.hash)
             self.head = new_block
+            print("adding to dict {}".format(self.head.hash))
             self.blockDict[self.head.hash] = self.head
         else:
-            self.head = Block(timestamp, data, 0)
+            self.head = Block(timestamp, data, None)
             self.blockDict[self.head.hash] = self.head
+        time.sleep(0.5)
 
     def print_chain(self):
         print("Printing chain:")
@@ -46,10 +50,16 @@ class BlockChain:
         current_block = self.head
         while current_block:
             yield (current_block.timestamp, current_block.data)
-            previous_block = self.blockDict.get(current_block.previous_hash, None)
+            current_block = self.blockDict.get(current_block.previous_hash, None)
 
 
 chain = BlockChain()
+
 chain.add_block("Transaction  01")
+
 chain.add_block("Transaction  02")
+
+chain.add_block("Transaction  03")
 chain.print_chain()
+print(chain.blockDict.items())
+print(chain.head.data)
